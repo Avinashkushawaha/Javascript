77ebc6a5e5e6eb5ebcf0d91c421afe7866aced63
Avinash kushawaha <Mauryaavinash085@gmail.com>
2024-11-08 00:10:41 +0530

    first commit

---
+++ Intro.js
@@ -0,0 +1,3 @@
+console.log("I am an 1 External Javascript");
+console.log("I am an 2 External Javascript");
+console.log("I am an 3 External Javascript");
---
+++ first_lecture.js
@@ -0,0 +1,18 @@
+var a; // declare
+a = 10; // initialisation
+a = 20; // re initialisation
+var a = 30; // re declare
+console.log(a);
+
+
+let b; // declare
+b = 30; //initialisation
+b = "hi" ; // re initialisation
+// let b = true; // !re declare
+console.log(b);
+
+
+const c = 100;
+// const c = "b"; // re declare
+c = "hi"; // !re initialisation
+console.log(c);
